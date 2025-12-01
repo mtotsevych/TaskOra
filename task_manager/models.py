cc2a108ef21cf5633bd4d7436dad48609b7755ff
Mykola Totsevych <totsevich02@gmail.com>
2025-12-01 23:50:17 +0100

    add Position, TaskType, and Tag models with __str__ methods and Meta ordering to task_manager/models.py

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
