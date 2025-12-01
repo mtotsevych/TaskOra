4fa68ca19d5e248e80f649258716b4fdf31f6467
Mykola Totsevych <totsevich02@gmail.com>
2025-12-01 23:39:37 +0100

    format all files with Black for consistent code style

--- TaskOra/asgi.py
+++ TaskOra/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaskOra.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TaskOra.settings")
 
 application = get_asgi_application()
--- TaskOra/wsgi.py
+++ TaskOra/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaskOra.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TaskOra.settings")
 
 application = get_wsgi_application()
