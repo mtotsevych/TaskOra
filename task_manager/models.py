462a7b87e2e1dc94e155d984e4d489aab4cb22ea
Mykola Totsevych <totsevich02@gmail.com>
2025-12-02 00:22:03 +0100

    add Task model with __str__ method; make Charfield 'name' unique in Position, TaskType, and Tag models

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,9 +1,10 @@
+from TaskOra import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class Position(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     class Meta:
         ordering = ("name",)
@@ -13,7 +14,7 @@ class Position(models.Model):
 
 
 class TaskType(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     class Meta:
         ordering = ("name",)
@@ -23,7 +24,7 @@ class TaskType(models.Model):
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     class Meta:
         ordering = ("name",)
@@ -41,3 +42,28 @@ class Worker(AbstractUser):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("low", "Low"),
+        ("medium", "Medium"),
+        ("high", "High"),
+        ("urgent", "Urgent"),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    deadline = models.DateField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+    assignees = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="tasks")
+
+    def __str__(self):
+        return self.name
