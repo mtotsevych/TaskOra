d594df9310192bd6fd89f830994d8e01cc03d205
Mykola Totsevych <totsevich02@gmail.com>
2025-12-02 00:00:25 +0100

    add Worker user model inheriting from AbstractUser with __str__ method and set AUTH_USER_MODEL in TaskOra/settings.py

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -29,3 +30,14 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name="workers"
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
