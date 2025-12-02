831e21d14481ec770daf83531e958f99ce0c2161
Mykola Totsevych <totsevich02@gmail.com>
2025-12-02 01:36:50 +0100

    populate all models with data, create superuser, and register models in task_manager/admin.py

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from task_manager.models import (
+    Position,
+    TaskType,
+    Tag,
+    Worker,
+    Task
+)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position", )
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("position",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("first_name", "last_name","position",)})
+    )
+    list_filter = UserAdmin.list_filter + ("position",)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "deadline", "is_completed", "priority", "task_type",)
+    list_filter = ("is_completed", "priority", "task_type",)
+    search_fields = ("name",)
+
+
+admin.site.register(Position)
+admin.site.register(TaskType)
+admin.site.register(Tag)
+
+admin.site.unregister(Group)
