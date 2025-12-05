865bd33c3a4af5504b8d2bc58644ecaecbcea778
Mykola Totsevych <totsevich02@gmail.com>
2025-12-05 03:18:39 +0100

    add CRUD routes for worker profiles in task_manager/urls.py: add profile list view route (/profiles/); add worker profile detail view (/profile/<int:pk>/); add worker profile update route (/profile/<int:pk>/update/); add worker profile delete route (/profile/<int:pk>/delete/)

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path
 
-from task_manager.views import WorkerCreateView, index
+from task_manager.views import (
+    index,
+    WorkerCreateView,
+    WorkerDetailView,
+    WorkerDeleteView,
+    WorkerUpdateView,
+    WorkerListView,
+)
 
 app_name = "task_manager"
 
 urlpatterns = [
     path("", index, name="index"),
     path("register/", WorkerCreateView.as_view(), name="register"),
+    path("profiles/", WorkerListView.as_view(), name="profile-list"),
+    path("profile/<int:pk>/", WorkerDetailView.as_view(), name="profile-detail"),
+    path("profile/<int:pk>/update/", WorkerUpdateView.as_view(), name="profile-update"),
+    path("profile/<int:pk>/delete/", WorkerDeleteView.as_view(), name="profile-delete"),
+
 ]
