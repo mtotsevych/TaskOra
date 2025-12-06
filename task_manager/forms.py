4b9086bca9e9d63a42dbb67893526ddb64e56b32
Mykola Totsevych <totsevich02@gmail.com>
2025-12-06 01:26:02 +0100

    add TaskCreationChangeForm for creating and updating tasks; add TaskSearchForm for searching tasks by name in task_manager/forms.py

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-from task_manager.models import Worker
+from task_manager.models import Worker, Task, Tag
 
 
 class WorkerCreationForm(UserCreationForm):
@@ -19,6 +20,51 @@ class WorkerSearchForm(forms.Form):
         label="",
         widget=forms.TextInput(attrs={
             "placeholder": "Search by username",
+            "class": "form-control",
+        })
+    )
+
+
+class TaskCreationChangeForm(forms.ModelForm):
+    deadline = forms.DateField(
+        required=False,
+        widget=forms.DateInput(attrs={"type": "date"})
+    )
+    tags = forms.ModelMultipleChoiceField(
+        required=False,
+        queryset=Tag.objects.all(),
+        widget=forms.SelectMultiple(attrs={
+            "style": "min-height: 120px;"
+        })
+    )
+    assignees = forms.ModelMultipleChoiceField(
+        required=False,
+        queryset=get_user_model().objects.all(),
+        widget=forms.SelectMultiple(attrs={
+            "style": "min-height: 120px;"
+        })
+    )
+
+    class Meta:
+        model = Task
+        fields = (
+            "name",
+            "description",
+            "deadline",
+            "priority",
+            "task_type",
+            "tags",
+            "assignees",
+        )
+
+
+class TaskSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={
+            "placeholder": "Search by task name",
             "class": "form-control",
         })
     )
