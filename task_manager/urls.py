fca3e89a224411d18df2860237cd47cf2ead8e97
Mykola Totsevych <totsevich02@gmail.com>
2025-12-06 01:22:52 +0100

    add full CRUD and status toggle routes for tasks

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -7,6 +7,12 @@ from task_manager.views import (
     WorkerDeleteView,
     WorkerUpdateView,
     WorkerListView,
+    TaskListView,
+    complete_and_incomplete_task,
+    TaskCreateView,
+    TaskDetailView,
+    TaskUpdateView,
+    TaskDeleteView
 )
 
 app_name = "task_manager"
@@ -18,5 +24,10 @@ urlpatterns = [
     path("profile/<int:pk>/", WorkerDetailView.as_view(), name="profile-detail"),
     path("profile/<int:pk>/update/", WorkerUpdateView.as_view(), name="profile-update"),
     path("profile/<int:pk>/delete/", WorkerDeleteView.as_view(), name="profile-delete"),
-
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>/complete-incomplete/", complete_and_incomplete_task, name="task-complete-incomplete"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
