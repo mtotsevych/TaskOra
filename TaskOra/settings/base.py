3d5f3949acf12dac65506a0e8b9132367e032ee4
Mykola Totsevych <totsevich02@gmail.com>
2025-12-08 23:30:45 +0100

    remove the single settings.py file and introduced a new settings package with base.py, development.py, and production.py modules [this separation isolates shared configuration from environment-specific settings and improves maintainability for development and production environments]

--- TaskOra/settings/base.py
+++ TaskOra/settings/base.py
@@ -9,14 +9,16 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+
 import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,12 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -42,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "crispy_forms",
     "crispy_bootstrap4",
-    "task_manager"
+    "task_manager",
 ]
 
 MIDDLEWARE = [
@@ -73,17 +69,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "TaskOra.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
